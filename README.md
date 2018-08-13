94c548ff72c0ee153bb26b50d0fa93b15455e63b
6adityag8 <adityag@mindfiresolutions.com>
2018-08-13 14:38:10 +0530

    authentication model

---
+++ README.md
@@ -0,0 +1,10 @@
+This repo is the practice for "Learning django and angularjs" from thinkster.io
+## Installation
+
+* `$ pip install -r requirements.txt`
+* `$ npm install -g bower`
+* `$ npm install`
+* `$ bower install`
+* `$ python manage.py migrate`
+* `$ python manage.py runserver`
+
---
+++ authentication/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+# Create your models here.
+
+class Account(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=40, unique=True)
+    first_name = models.CharField(max_length=40, blank=True)
+    last_name = models.CharField(max_length=40, blank=True)
+    tagline = models.CharField(max_length=40, blank=True)
+    is_admin = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = AccountManager()
+    
