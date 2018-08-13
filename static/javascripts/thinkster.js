45b8601d4c5cdcb56826aedcd1ac610d96839ffa
6adityag8 <adityag@mindfiresolutions.com>
2018-08-13 19:52:03 +0530

    CSRF protection

--- static/javascripts/thinkster.js
+++ static/javascripts/thinkster.js
@@ -6,7 +6,15 @@
       'thinkster.config',
       'thinkster.routes',
       'thinkster.authentication',
-    ]);
+    ])
+    .run(run);
+
+  run.$inject = ['$http'];
+
+  function run($http) {
+    $http.defaults.xsrfHeaderName = 'X-CSRFToken';
+    $http.defaults.xsrfCookieName = 'csrftoken';
+  }
 
   angular
     .module('thinkster.routes', ['ngRoute']);
