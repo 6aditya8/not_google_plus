25d0ee198a8c130b4fd2fc231fd58d3bb5286b34
6adityag8 <adityag@mindfiresolutions.com>
2018-08-14 15:09:15 +0530

    add LoginView

--- authentication/views.py
+++ authentication/views.py
@@ -1,4 +1,10 @@
+import json
+
+from django.contrib.auth import authenticate, login
+
 from rest_framework import permissions,viewsets
+from rest_framework.response import Response
+from rest_framework import status, views
 
 from .models import Account
 from .serializers import AccountSerializer
@@ -32,3 +38,32 @@ class AccountViewSet(viewsets.ModelViewSet):
                         'status': 'Bad Request',
                         'message': "Account couldn't be created"
         }, status = status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(views.APIView):
+    
+    def post(self, request, format = None):
+        data = json.loads(request.body)
+        email = data.get('email', None)
+        password = data.get('password', None)
+        account = authenticate(email = email, password = password)
+
+        if account is not None:
+
+            if account.is_active:
+                login(request, account)
+                serialized = AccountSerializer(account)
+                
+                return Response(serialized.data)
+
+            else:
+                return Response({
+                    'status':'UnAuthorized',
+                    'message': 'This account has been disabled',
+                }, status = tatus.HTTP_401_UNAUTHORIZED)
+
+        else:
+            return Response({
+                    'status':'UnAuthorized',
+                    'message': 'Username or Password is incorrect',
+                }, status = tatus.HTTP_401_UNAUTHORIZED)
